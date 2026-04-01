97a4b393d20fa0ea38a9fa76e96e83d9cf3ea09d
Neovix33 <112022312+Neovix33@users.noreply.github.com>
2026-03-31 23:04:43 -0300

    agregar string en pantalla

--- app/src/main/java/com/example/tarea1/MainActivity.kt
+++ app/src/main/java/com/example/tarea1/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarea1.ui.theme.Tarea1Theme
 
@@ -37,7 +38,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! \n ${stringResource(id = R.string.bienvenido)}.\n ${stringResource(id = R.string.explorar)}. \n ${stringResource(id = R.string.tercer)}. ",
         modifier = modifier
     )
 }
